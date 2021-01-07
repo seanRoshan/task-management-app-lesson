b1ee2f324fc0d17993b37e018a822ad218c55112
SEAN ROSHAN <sroshan@dentalxchange.com>
2021-01-06 16:06:45 -0800

    feat: initial commit

---
+++ src/test/java/com/seanroshan/taskmanagementapplesson/TaskManagementAppLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanroshan.taskmanagementapplesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
